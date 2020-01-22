ea969735805a46a221f00e5a110ecf9f6f3770ed
IkedaNatsuo <60025825+IkedaNatsuo@users.noreply.github.com>
2020-01-22 17:56:12 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,19 @@
 int main(int argc, char *argv[]) 
 {
     char str[5];
+    int n = 10;
+    int*a = malloc(sizeof(int)*n);
+    
+    int count = 0;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        int value = scanf("%f",str);
+        if(value >= 0) {
+            a[count] = value;
+            count++;
+        } else {
+            printf("%s", a[value]);
+        }
     }
 
     return 0;
